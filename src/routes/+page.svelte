5514e0f4a27240ac7d8f016ab53926fa592cc737
evariste1963 <evariste@hotmail.co.uk>
2022-11-18 20:28:00 +0000

    before Card component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 	{#each movies as movie}
 		<img src={`https:/image.tmdb.org/t/p/w200/${movie.poster_path}`} alt="movie.title" />
 		<h3>{movie.title}</h3>
-		<h5>{movie.overview}</h5>
+		<p>{movie.overview}</p>
 		<h4>released: {movie.release_date}</h4>
 		<br />
 		<hr />
