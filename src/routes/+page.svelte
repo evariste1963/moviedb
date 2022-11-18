f2f52b463054db646dec372aeb222e953314d631
evariste1963 <65227896+evariste1963@users.noreply.github.com>
2022-11-18 08:04:26 +0000

    image trial done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,16 @@
 <script>
 	export let data;
 	const { movies } = data;
-	// console.log(movies[0]);
+	console.log(movies[0]);
 </script>
 
 <ul>
 	{#each movies as movie}
+		<img src={`https:/image.tmdb.org/t/p/w200/${movie.poster_path}`} alt="movie.title" />
 		<h3>{movie.title}</h3>
 		<h5>{movie.overview}</h5>
 		<h4>released: {movie.release_date}</h4>
 		<br />
+		<hr />
 	{/each}
 </ul>
