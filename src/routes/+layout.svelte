e70d93194dbf4e33dcc0e782e84803dba72e4f17
evariste1963 <evariste@hotmail.co.uk>
2022-11-25 06:11:46 +0000

    pre-rendering links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <nav>
 	<button><a href="/">Home</a></button>
-	<button><a href="/Movies/PopularMovies ">Popular Movies</a></button>
-	<button><a href="/Movies/TopRatedMovies ">Top Rated Movies</a></button>
+	<button><a href="/Movies/PopularMovies" data-sveltekit-prefetch>Popular Movies</a></button>
+	<button><a href="/Movies/TopRatedMovies" data-sveltekit-prefetch>Top Rated Movies</a></button>
 </nav>
 <slot />
 
