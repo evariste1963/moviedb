93e43d0471307dec5f6e8f0ea3ffd441233249f3
evariste1963 <65227896+evariste1963@users.noreply.github.com>
2022-11-25 14:48:53 +0000

    changed language to french

--- src/routes/Movies/PopularMovies/+page.js
+++ src/routes/Movies/PopularMovies/+page.js
@@ -2,11 +2,11 @@ let API_KEY = '0e04b7df85fdc659ca268b3647959793';
 
 export async function load({ fetch }) {
 	let res = await fetch(
-		`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+		`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=fr&page=1`
 	);
 	let movieObj = await res.json();
 	let movies = movieObj.results;
-
+	console.log(movies);
 	if (movies) {
 		return {
 			movies
