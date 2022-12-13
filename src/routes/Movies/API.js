494afefebe6dac59eb4d2e3e6728b7c4822ecb51
evariste1963 <65227896+evariste1963@users.noreply.github.com>
2022-12-13 16:04:43 +0000

    [ager restored

--- src/routes/Movies/API.js
+++ src/routes/Movies/API.js
@@ -1,8 +1,8 @@
 let API_KEY = '0e04b7df85fdc659ca268b3647959793';
-let page = 1;
-const AJAX = async function ({ fetch }, pathname, page) {
+let pager = 1;
+const AJAX = async function ({ fetch }, pathname, pager) {
 	const res = await fetch(
-		`https://api.themoviedb.org/3/movie/${pathname}?api_key=${API_KEY}&language=fr&page=${page}`
+		`https://api.themoviedb.org/3/movie/${pathname}?api_key=${API_KEY}&language=fr&page=${pager}`
 	);
 	let movieObj = await res.json();
 	return movieObj;
