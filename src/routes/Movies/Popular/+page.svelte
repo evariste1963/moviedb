77ea82681f11faf2eaadf1f6026a83d5a5268c2b
evariste1963 <65227896+evariste1963@users.noreply.github.com>
2022-12-15 09:27:14 +0000

    removed {#key value}

--- src/routes/Movies/Popular/+page.svelte
+++ src/routes/Movies/Popular/+page.svelte
@@ -2,41 +2,36 @@
 	import MovieTemplate from '../MovieTemplate.svelte';
 	import { page } from '$app/stores';
 	//import AJAX from '../API';
-	
+
 	export let data;
 
 	let { pathname, pager } = data;
-	
 
 	// create page & prev next functions here? __ or on +page.js??
 	export async function nextPage() {
 		console.log('the next page is:', pager + 1);
-			let urlref =
-				'https://api.themoviedb.org/3/movie' +
-				$page.url.pathname.slice(7).toLowerCase() +
-				'?api_key=0e04b7df85fdc659ca268b3647959793&language=fr&page=' +
-				(pager + 4);
+		let urlref =
+			'https://api.themoviedb.org/3/movie' +
+			$page.url.pathname.slice(7).toLowerCase() +
+			'?api_key=0e04b7df85fdc659ca268b3647959793&language=fr&page=' +
+			(pager + 4);
 
-			const res = await fetch(urlref);
-			console.log('this is the res', urlref, res);
-			let results = await res.json();
-			let data = results.results
-			pager++;
-			console.log('this is the data', data);
-			
-			
-			
-		// 	if (data) {
-		// 	return {
-		// 		data,
-				
-		// 	};
-		// }
-		
+		const res = await fetch(urlref);
+		console.log('this is the res', urlref, res);
+		let results = await res.json();
+		let data = results.results;
+		pager++;
+		console.log('this is the data', data);
+
+		if (data) {
+			return {
+				data
+			};
+		}
 	}
-	
 </script>
 
-{#key value}
-<MovieTemplate {data} on:click={nextPage} /> 
-{/key}
+<!-- {#key value}
+<MovieTemplate {data} on:click={nextPage} />
+{/key} -->
+<MovieTemplate {data} on:click={nextPage} />
