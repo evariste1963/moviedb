7a46691d1df57735f388449580be481083fe374c
evariste1963 <evariste@hotmail.co.uk>
2022-12-17 20:18:04 +0000

    trial goto repo

--- src/routes/Movies/[movieid]/+page.js
+++ src/routes/Movies/[movieid]/+page.js
@@ -1,12 +1,12 @@
 //https://api.themoviedb.org/3/movie/{movie_id}?api_key=<<api_key>>&language=fr
 let API_KEY = '0e04b7df85fdc659ca268b3647959793';
-
+console.log('paramsid', params.movieid);
 export async function load({ fetch, params }) {
 	let res = await fetch(
 		`https://api.themoviedb.org/3/movie/${params.movieid}?api_key=${API_KEY}&language=fr&page=1`
 	);
 	let movie = await res.json();
-	//console.log('movie', movie);
+	console.log('movie', movie);
 	if (movie) {
 		return {
 			movie
