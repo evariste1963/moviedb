dc8c5f39f3c035fc6976c40188a018fd1f80ec19
evariste1963 <65227896+evariste1963@users.noreply.github.com>
2022-12-21 15:46:57 +0000

    moved single movie detal to lib/shared SingleMovie.svelte

--- src/routes/Movies/[movieid]/+page.server.js
+++ src/routes/Movies/[movieid]/+page.server.js
@@ -2,7 +2,6 @@ import AJAX from '../API.js';
 
 export async function load({ fetch, params }) {
 	let pathname = params.movieid;
-	console.log(pathname);
 	let movie = await AJAX({ fetch }, pathname);
 
 	if (movie) {
