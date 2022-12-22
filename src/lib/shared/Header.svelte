e2b3aaf78d6f4f5f3de11274eb827a64bd6bf6f4
evariste1963 <evariste@hotmail.co.uk>
2022-12-22 22:11:42 +0000

    tmdb logo added

---
+++ src/lib/shared/Header.svelte
@@ -0,0 +1,30 @@
+<div class="header">
+	<img src="src\lib\images\tmdb.svg" alt="tmdb" />
+	<div class="banner">
+		<h1>MovieDb Movie Database</h1>
+	</div>
+</div>
+
+<style>
+	.header {
+		position: sticky;
+		display: flex;
+		top: 0;
+		width: 100%;
+		background-color: black;
+		height: 3rem;
+		color: rgb(247, 226, 200);
+	}
+
+	img {
+		width: 10rem;
+		margin-left: 2rem;
+	}
+
+	.banner {
+		margin: auto;
+	}
+	.header h1 {
+		margin: 0;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,22 @@
 <script>
 	import '../../src/app.css';
+	import Header from '$lib/shared/Header.svelte';
 	import Navbar from '$lib/shared/Navbar.svelte';
 	import Footer from '$lib/shared/Footer.svelte';
 </script>
 
-<Navbar />
-<slot />
-<Footer />
+<Header />
+<div class="bodySection">
+	<Navbar />
+	<slot />
+	<Footer />
+</div>
+
+<style>
+	.bodySection {
+		width: 80%;
+		margin: 0 auto;
+		border-left: 3px solid #373b44;
+		border-right: 3px solid #373b44;
+	}
+</style>
