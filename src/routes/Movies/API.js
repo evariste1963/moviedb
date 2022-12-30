49e8d7b3b2f86a590779304227a4bff5f27beb70
evariste1963 <evariste@hotmail.co.uk>
2022-12-30 20:33:55 +0000

    .env file created for API key--> made secret

--- src/routes/Movies/API.js
+++ src/routes/Movies/API.js
@@ -1,8 +1,8 @@
-let API_KEY = '0e04b7df85fdc659ca268b3647959793';
+import { SECRET_API_KEY } from '$env/static/private';
 
 const AJAX = async function ({ fetch }, pathname, currentPage = 1) {
 	const res = await fetch(
-		`https://api.themoviedb.org/3/movie/${pathname}?api_key=${API_KEY}&language=fr&page=${currentPage}&nclude_adult=true`
+		`https://api.themoviedb.org/3/movie/${pathname}?api_key=${SECRET_API_KEY}&language=fr&page=${currentPage}&nclude_adult=true`
 	);
 	let movieObj = await res.json();
 	return movieObj;
--- src/routes/[search]/+page.server.js
+++ src/routes/[search]/+page.server.js
@@ -1,10 +1,11 @@
-let API_KEY = '0e04b7df85fdc659ca268b3647959793';
+import { SECRET_API_KEY } from '$env/static/private';
+
 
 export async function load({ fetch, params }) {
 	let { search } = params;
 
 	const res = await fetch(
-		`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=fr&query=${search}&page=1&nclude_adult=true`
+		`https://api.themoviedb.org/3/search/movie?api_key=${SECRET_API_KEY}&language=fr&query=${search}&page=1&nclude_adult=true`
 	);
 
 	let movieObj = await res.json();
